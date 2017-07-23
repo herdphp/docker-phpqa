47a2cf05cff8ad0c2213ac0ebe29594934bf1ff8
brunoric <brunoric@gmail.com>
2017-07-23 18:35:59 +0100

    Changing workdir of generated command

--- bin/phpqa.sh
+++ bin/phpqa.sh
@@ -149,8 +149,8 @@ function executeGenerate()
 {
     parseGenerateArgs ${_COMMAND_ARGS};
     fixGenerateDir;
-    docker run --rm -i -t -v ${_GENERATE_DIR}:/usr/src/phpt herdphp/phpqa:${_GENERATE_VERSION} \
-        php scripts/dev/generate-phpt.phar ${_GENERATE_ARGS} | sed "s/php generate-phpt.php /.\/phpqa/";
+    docker run --rm -i -t -w /usr/src/phpt -v ${_GENERATE_DIR}:/usr/src/phpt herdphp/phpqa:${_GENERATE_VERSION} \
+        php /usr/src/php/scripts/dev/generate-phpt.phar ${_GENERATE_ARGS} | sed "s/php generate-phpt.php /.\/phpqa/";
 }
 
 function executeCommand()
